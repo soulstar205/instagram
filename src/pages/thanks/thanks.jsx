0f2a450b6c06c356e6733736a155435ddf6f900d
soulstar205 <soulstar205@gmail.com>
2023-02-01 07:32:20 +0100

    thank you page

--- src/pages/thanks/thanks.jsx
+++ src/pages/thanks/thanks.jsx
@@ -4,7 +4,7 @@ import './thanks.css';
 function ThankYouPage() {
   return (
     <div>
-      <h1 className="fancy-text center-text">Thank you for your vote, we love you!</h1>
+      <h1 className="fancy-text center-text">Thank you for voting, we love you!</h1>
     </div>
   );
 }
